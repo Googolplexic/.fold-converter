f45244fed8141cdfc4d2cd48a1596236695a13ed
Googolplexic <colemancflai@yahoo.com>
2024-06-05 16:38:05 -0700

    Added corner checking and fixed vertex shifting

--- exclusion.js
+++ exclusion.js
@@ -2,7 +2,7 @@ console.log('exclusion.js loaded');
 
 function shiftVertices(jsonData, vertexToRemove) {
     for (let edge of jsonData.edges_vertices) {
-        for (let i = 0; i < edge.length; i++) {
+        for (let i = 0; i < 2; i++) {
             if (edge[i] > vertexToRemove) {
                 edge[i]--;
             }
@@ -13,23 +13,54 @@ function shiftVertices(jsonData, vertexToRemove) {
 function excludeVertex(jsonData, vertexToRemove, edge1, edge2) {
     const edge1Index = jsonData.edges_vertices[edge1].indexOf(vertexToRemove);
     const edge2Index = jsonData.edges_vertices[edge2].indexOf(vertexToRemove) == 0 ? 1 : 0;
-
     jsonData.edges_vertices[edge1][edge1Index] = jsonData.edges_vertices[edge2][edge2Index];
-
     jsonData.edges_vertices.splice(edge2, 1);
     jsonData.edges_assignment.splice(edge2, 1);
     jsonData.edges_foldAngle.splice(edge2, 1);
-
-
     jsonData.vertices_coords.splice(vertexToRemove, 1);
-
     shiftVertices(jsonData, vertexToRemove);
+}
+
+function isCorner(jsonData, targetVertex, edge1, edge2, elipson) {
+    if (!jsonData.edges_assignment[edge1] == 'B' || !jsonData.edges_assignment[edge2] == 'B') {
+        return false;
+    }
+    const targetVertexX = jsonData.vertices_coords[targetVertex][0];
+    const targetVertexY = jsonData.vertices_coords[targetVertex][1];
+    let minValue = [targetVertexX, targetVertexY];
+    let maxValue = [targetVertexX, targetVertexY];
+    for (let vertex of jsonData.vertices_coords) {
+        if (vertex[0] > maxValue[0]) { maxValue[0] = vertex[0]; }
+        if (vertex[1] > maxValue[1]) { maxValue[1] = vertex[1]; }
+        if (vertex[0] < minValue[0]) { minValue[0] = vertex[0]; }
+        if (vertex[1] < minValue[1]) { minValue[1] = vertex[1]; }
+    }
+    if (targetVertexX + elipson > maxValue[0] && targetVertexY + elipson > maxValue[1]
+        || targetVertexX + elipson > maxValue[0] && targetVertexY - elipson < minValue[1]
+        || targetVertexX - elipson < minValue[0] && targetVertexY + elipson > maxValue[1]
+        || targetVertexX - elipson < minValue[0] && targetVertexY - elipson < minValue[1]
+    ) { return true; }
+    else { return false; }
+}
+
+function shouldBeRemoved(jsonData, vertexToRemove, edge1, edge2) {
+    if (isCorner(jsonData, vertexToRemove, edge1, edge2, 0.001)) {
+        console.log("Vertex is a corner, will ignore.\n")
+        return false;
+    }
+    if (jsonData.edges_assignment[edge1] != jsonData.edges_assignment[edge2]) {
+        console.log("Edges are different orientation, cannot exclude vertex.\n")
+        return false;
+    }
+    console.log("Removing vertex\n");
+    return true;
 }
 
 function findExclusions(jsonData) {
     let numVertices = Object.keys(jsonData.vertices_coords).length;
     let numEdges = Object.keys(jsonData.edges_vertices).length;
     for (let i = 0; i < numVertices; i++) {
+        console.log("Looking at vertex" + i + "\n");
         let vertexDegree = 0;
         let edge1 = -1;
         let edge2 = -1;
@@ -39,13 +70,10 @@ function findExclusions(jsonData) {
                 (edge1 == -1) ? edge1 = j : edge2 = j;
             }
         }
-        if (vertexDegree == 2 && jsonData.edges_assignment[edge1] == jsonData.edges_assignment[edge2]) {
-            console.log(i + '\n');
-            console.log(edge1 + '\n');
-            console.log(edge2 + '\n');
-            console.log(jsonData.edges_vertices[edge2][1]);
+        if (vertexDegree == 2 && shouldBeRemoved(jsonData, i, edge1, edge2)) {
             excludeVertex(jsonData, i, edge1, edge2);
             numEdges--;
+            i--;
             numVertices--;
         }
     }
